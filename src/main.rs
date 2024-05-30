f8946a80886641732b775b9a429d20fc54943276
Carl <299418+Vadoola@users.noreply.github.com>
2024-05-30 04:35:43 +0000

    Updated to a minimum working example for a slide out on the model.
    The items that dissapear will slide out to the left when they are filtered out.
    This works by using a timer in the Rust code before calling filter_model.reset()
    essentially when the hide property gets toggled the animation gets triggered in Slint
    which lasts for 250s. When the property gets toggled a 500ms timer starts in Rust,
    and when that timer is done it calls filter_model.reset(). This works perfectly for
    items that are going away.
    
    However so far none of my attempts work on getting something similar for items that
    are showing back up. No matter what I have tried they always just appear.

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,11 @@ fn main() -> Result<(), slint::PlatformError> {
         },
         mod_str {
             text: "Text 2".into(),
-            hidden: true,
+            hidden: false,
         },
         mod_str {
             text: "Text 3".into(),
-            hidden: false,
+            hidden: true,
         },
         mod_str {
             text: "Text 4".into(),
@@ -27,22 +27,28 @@ fn main() -> Result<(), slint::PlatformError> {
     ]));
 
     let filt_ui = ui.as_weak();
-    let filt_mod = ModelRc::from(model.clone()).filter(move |fmod| !(filt_ui.unwrap().get_hide() && fmod.hidden));
+    let filt_mod = ModelRc::from(model.clone())
+        .filter(move |fmod| !(filt_ui.unwrap().get_hide() && fmod.hidden));
     let filt_mod = Rc::new(filt_mod);
     ui.set_list_mod(ModelRc::from(filt_mod.clone()));
 
     let timer = Timer::default();
+    let timer_ui = ui.as_weak();
     ui.on_up_mod(move || {
+        let timer_ui = timer_ui.unwrap();
         let filt_mod = filt_mod.clone();
-        
-        timer.start(
-            slint::TimerMode::SingleShot,
-            std::time::Duration::from_millis(1000),
-            move || {
-                filt_mod.reset();
 
-            },
-        );
+        if timer_ui.get_hide() {
+            timer.start(
+                slint::TimerMode::SingleShot,
+                std::time::Duration::from_millis(500),
+                move || {
+                    filt_mod.reset();
+                },
+            );
+        } else {
+            filt_mod.reset();
+        }
     });
 
     ui.run()
