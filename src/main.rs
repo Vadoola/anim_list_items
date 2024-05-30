afea08b283ce2a7b10bdf7afc68afc50436250fd
Carl <299418+Vadoola@users.noreply.github.com>
2024-05-30 15:18:45 +1200

    Update main.rs
    Making some adjustments and working on a new idea, that kind of seems to work for a fade in / fade out when the model is filtered

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use slint::{ModelExt, ModelRc, VecModel};
+use slint::{ModelExt, ModelRc, Timer, VecModel};
 
 slint::include_modules!();
 
@@ -10,7 +10,7 @@ fn main() -> Result<(), slint::PlatformError> {
     let model = Rc::new(VecModel::from(vec![
         mod_str {
             text: "Text 1".into(),
-            hidden: false,
+            hidden: true,
         },
         mod_str {
             text: "Text 2".into(),
@@ -25,17 +25,24 @@ fn main() -> Result<(), slint::PlatformError> {
             hidden: false,
         },
     ]));
-    ui.set_list_mod(ModelRc::from(model.clone()));
 
-    let callb_handle = ui.as_weak();
-    let omod = model.clone();
+    let filt_ui = ui.as_weak();
+    let filt_mod = ModelRc::from(model.clone()).filter(move |fmod| !(filt_ui.unwrap().get_hide() && fmod.hidden));
+    let filt_mod = Rc::new(filt_mod);
+    ui.set_list_mod(ModelRc::from(filt_mod.clone()));
+
+    let timer = Timer::default();
     ui.on_up_mod(move || {
-        let callb_handle = callb_handle.unwrap();
-        let hide = callb_handle.get_hide();
-        let filt_mod = ModelRc::from(omod.clone()).filter(move |fmod| {
-            !(hide && fmod.hidden)
-        });
-        callb_handle.set_list_mod(ModelRc::from(Rc::new(filt_mod)));
+        let filt_mod = filt_mod.clone();
+        
+        timer.start(
+            slint::TimerMode::SingleShot,
+            std::time::Duration::from_millis(1000),
+            move || {
+                filt_mod.reset();
+
+            },
+        );
     });
 
     ui.run()
